993238db9ca0cc3367605d7409ffdab323d2ad88
Giobeshara <giobe@DESKTOP-2CE88N8>
2025-03-11 15:34:42 +0100

    alt virker, der skal laves downvote nu

--- Blazor-app/obj/Debug/net7.0/BlazorApp1.AssemblyInfo.cs
+++ Blazor-app/obj/Debug/net7.0/BlazorApp1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BlazorApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2f81e3a2368d78b5ff3ced2fea64be72106a0c43")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8f289aa7acbfb4ef1b48729f34724889360731e6")]
 [assembly: System.Reflection.AssemblyProductAttribute("BlazorApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BlazorApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
